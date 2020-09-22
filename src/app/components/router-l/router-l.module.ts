e88cafee28b5594da0c714039cc10b3e05297481
Alex <glopezalex631@gmail.com>
2020-09-22 15:10:42 -0500

    Rutas

--- src/app/components/router-l/router-l.module.ts
+++ src/app/components/router-l/router-l.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from '../login/login.component';
 
-//Declaracion de rutas
+// Declaracion de rutas
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'rondas', component: RondasComponent },
-  { path: 'equipos', component: EquiposComponent }
+  // { path: 'rondas', component: RondasComponent },
+  // { path: 'equipos', component: EquiposComponent }
 ];
 
 @NgModule({
