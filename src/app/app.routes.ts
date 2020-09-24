a76eb00711757d0fec2d0a2d115b9918253faaff
Alex <glopezalex631@gmail.com>
2020-09-24 17:51:20 -0500

    Login y Registro
    Feos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {RouterModule, Routes} from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 const APP_ROUTES: Routes = [
     {path: 'Login', component: LoginComponent},
+    {path: 'Registro', component: RegistroComponent},
     {path: '**', pathMatch: 'full', redirectTo: 'Login'}
 ];
 
